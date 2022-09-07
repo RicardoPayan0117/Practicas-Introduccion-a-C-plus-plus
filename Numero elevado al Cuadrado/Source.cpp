c3acc92f8b478ecddf005c52421a0dc1c2de0711
ricardopayan0117 <ricardopayan0117@gmail.com>
2022-09-06 21:13:25 -0600

    Primeras practicas

---
+++ Numero elevado al Cuadrado/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+
+    double numero = 7;
+    double elevado = pow(numero, 2);
+    cout << "El numero " << numero << " elevado al cuadrado es " << elevado;
+    return 0;
+
+}
